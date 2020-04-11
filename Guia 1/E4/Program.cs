4f4141389b7b7cfacb8c79c46184360d2a48b328
BrahianSozaki <brahian.2003@yahoo.com.ar>
2020-04-11 19:55:05 -0300

    Cambie el 4 porque no me fije que te pedia un menu para las acciones del gato

--- Guia 1/E4/Program.cs
+++ Guia 1/E4/Program.cs
@@ -8,23 +8,50 @@ namespace Programas
         static void Main(string[] args)
         {
 
-            Gatito umbra = new Gatito ("Umbra", true, 100);
+            Gatito gato = new Gatito ("Umbra", true, 100);
 
-            Console.WriteLine(umbra.Nombre());
+            int opcion = 0;
 
-            if(umbra.EstaVacunado()){
-                Console.WriteLine("Esta vacunado");
-            }else{
-                Console.WriteLine("No esta vacunado");
-            }
+            while (opcion != 6){
 
-            umbra.jugar(5);
-            umbra.Comer();
+                Console.WriteLine("[1]Nombre del gatito\n[2]Saber si esta o no vacunado\n[3]Jugar\n[4]Comer\n[5]Saber si esta saludable\n[6]Salir");
+                opcion = Int32.Parse(Console.ReadLine());
 
-            if(umbra.estaSaludable()){
-                Console.WriteLine("Esta saludable");
-            }else{
-                Console.WriteLine("No esta saludable");
+                switch (opcion){
+
+                    case 1:
+                        Console.WriteLine(gato.Nombre());
+                        break;
+
+                    case 2:
+                        if(gato.EstaVacunado()){
+                            Console.WriteLine("Esta vacunado");
+                        }else{
+                            Console.WriteLine("No esta vacunado");
+                        }
+                        break;
+
+                    case 3:
+                        int minutos;
+                        Console.WriteLine("Ingrese los minutos de juego");
+                        minutos = Int32.Parse(Console.ReadLine());
+                        gato.jugar(minutos);
+                        break;
+
+                    case 4:
+                        gato.Comer();
+                        break;
+
+                    case 5:
+                        if(gato.estaSaludable()){
+                            Console.WriteLine("Esta saludable");
+                        }else{
+                            Console.WriteLine("No esta saludable");
+                        }
+                        break;
+
+                }
+
             }
 
         }
