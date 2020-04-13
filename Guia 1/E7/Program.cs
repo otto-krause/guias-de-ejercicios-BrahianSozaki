b2baa3dbb3996f9917c749095b59e2a018f07be2
BrahianSozaki <brahian.2003@yahoo.com.ar>
2020-04-13 15:53:35 -0300

    Agregue un menu al 7 por si las dudas, no lo decia como tal pero supuse que tenia que interactuar el usuario asi que lo coloque

--- Guia 1/E7/Program.cs
+++ Guia 1/E7/Program.cs
@@ -10,11 +10,29 @@ namespace Programas
 
             Celular celular = new Celular(true,true,20000,2);
 
-            Console.WriteLine("La cantidad de memoria disponible es:"+celular.MemoriaDisponible());
-            if (celular.TeCargaLaSube()){
-                Console.WriteLine("Si te carga la sube");
-            }else{
-                Console.WriteLine("No te carga la sube");
+            int opcion = 0;
+
+            while (opcion != 3){
+
+                Console.WriteLine("[1]Ver memoria disponible en el celular\n[2]Saber si te carga la sube\n[3]Salir");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    
+                    case 1:
+                        Console.WriteLine("La cantidad de memoria disponible es:"+celular.MemoriaDisponible());
+                        break;
+
+                    case 2:
+                        if (celular.TeCargaLaSube()){
+                            Console.WriteLine("Si te carga la sube");
+                        }else{
+                            Console.WriteLine("No te carga la sube");
+                        }
+                        break;
+                        
+                }
+
             }
             
         }
