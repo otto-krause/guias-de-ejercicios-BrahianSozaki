32c4bb3add078e3906d53f5cfd2a4ff734c5390e
Brahian Rodriguez <Brahian.2003@yahoo.com.ar>
2020-05-26 23:37:07 -0300

    Hice el 1

---
+++ Guia 4/E1/Perro.cs
@@ -0,0 +1,14 @@
+namespace E1
+{
+    public class Perro : Animales
+    {
+        public Perro(int energia) : base(energia)
+        {
+
+        }
+        public override void comer() => energia += 15;
+        public override void jugar(){
+            if (energia >= 20) energia -= 20;
+        }
+    }
+}
--- Guia 4/E1/Program.cs
+++ Guia 4/E1/Program.cs
@@ -6,7 +6,38 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animales perro = new Perro(100);
+            Animales pajaro = new Pajaro(100);
+
+            int opcion = 0;
+
+            while(opcion <= 5){
+
+                Console.WriteLine("[1]Darle de comer al Perro\n[2]Darle de comer al Pajaro\n[3]Jugar con el Perro\n[4]Jugar con el Pajaro\n[5]Los animales duermen");
+                opcion = Int32.Parse(Console.ReadLine());
+
+                switch (opcion){
+                    case 1:
+                        perro.comer();
+                        break;
+                    case 2:
+                        pajaro.comer();
+                        break;
+                    case 3:
+                        perro.jugar();
+                        break;
+                    case 4:
+                        pajaro.jugar();
+                        break;
+                    case 5:
+                        perro.dormir();
+                        pajaro.dormir();
+                        break;
+                }
+                Console.WriteLine("El perro posee: " + perro.Energia + "de energia restante");
+                Console.WriteLine("El pajaro posee: " + pajaro.Energia + "de energia restante");
+
+            }
         }
     }
 }
