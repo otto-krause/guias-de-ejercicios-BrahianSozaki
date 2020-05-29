1b63b547c277657ed2aa1a3f9bfe3caceb4b8bc4
Brahian Rodriguez <Brahian.2003@yahoo.com.ar>
2020-05-28 21:19:01 -0300

    Le agregue un menu en la main

--- Guia 4/E5/Program.cs
+++ Guia 4/E5/Program.cs
@@ -6,7 +6,45 @@ namespace E5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SerPensante robot = new Robot(50);
+            SerPensante humano = new Humano(50);
+            SerPensante cyborg = new Cyborg(50);
+            string opcion = "";
+
+            while (opcion != "Salir"){
+                Console.WriteLine("[1]Hacer pensar al Robot\n[2]Hacer estudiar al Robot\n[3]Hacer pensar al Humano\n[4]Hacer estudiar al humano\n[5]Hacer pensar al Cyborg\n[6]Hacer estudiar al Cyborg");
+                opcion = Console.ReadLine();
+                switch (opcion){
+                    case "1":
+                        robot.pensar();
+                        break;
+                    case "2":
+                        Console.WriteLine("Ingrese que estudio");
+                        opcion = Console.ReadLine();
+                        robot.estudiar(opcion);
+                        break;
+                    case "3":
+                        humano.pensar();
+                        break;
+                    case "4":
+                        Console.WriteLine("Ingrese que estudio");
+                        opcion = Console.ReadLine();
+                        humano.estudiar(opcion);
+                        break;
+                    case "5":
+                        cyborg.pensar();
+                        break;
+                    case "6":
+                        Console.WriteLine("Ingrese que estudio");
+                        opcion = Console.ReadLine();
+                        cyborg.estudiar(opcion);
+                        break;
+                    default:
+                        opcion = "Salir";
+                        break;
+                }
+            }
+
         }
     }
 }
