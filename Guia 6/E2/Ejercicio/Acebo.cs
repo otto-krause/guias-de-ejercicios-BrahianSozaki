7c4d1c6482dfff9d4a5217c694aa36502f575483
Brahian Rodriguez <Brahian.2003@yahoo.com.ar>
2020-08-16 15:59:35 -0300

    En el test no se reconocen/encuentran mago y receta

---
+++ Guia 6/E2/Ejercicio/Acebo.cs
@@ -0,0 +1,12 @@
+namespace Ejercicio
+{
+    public class Acebo : Varita
+    {
+        public Acebo(int potencia, Receta receta) : base(potencia, receta)
+        {
+        }
+        public override int tiempoParaLaReceta(){
+            return receta.Ingredientes() + potencia;
+        }
+    }
+}
